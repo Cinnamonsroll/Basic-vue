b0db068644c683ba7f0a0ba35c00d74fb57ad5c1
Cinnamonsroll <71993095+Cinnamonsroll@users.noreply.github.com>
2021-06-14 05:55:23 -0400

    Create main.vue

---
+++ frontend/src/components/main.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="test" class="btn btn-success">Submit</button>
+</template>
+<script>
+import httpModule from "../services/httpModule";
+export default {
+  name: "meow",
+  data() {
+    return {
+      tutorial: {
+        id: null,
+        title: "",
+        description: "",
+        published: false
+      },
+      submitted: false
+    };
+  },
+  methods: {
+    test: function test() {
+      httpModule.test().then((d) => console.log(d.data.data));
+    },
+  },
+};
+</script>
